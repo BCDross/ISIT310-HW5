ccf8bd4b6abda503aace5f3aa33efd922f0f5b1e
Darren Ross <darren.ross@bellevuecollege.edu>
2021-02-14 01:22:05 -0800

    Completed homework.

---
+++ ISIT310-HW5/ISIT310-HW5/Form.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ISIT310_HW5
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnUpdateCount = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblNewCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(557, 333);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btnUpdateCount
+            // 
+            this.btnUpdateCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateCount.Location = new System.Drawing.Point(419, 369);
+            this.btnUpdateCount.Name = "btnUpdateCount";
+            this.btnUpdateCount.Size = new System.Drawing.Size(123, 56);
+            this.btnUpdateCount.TabIndex = 1;
+            this.btnUpdateCount.Text = "Update Count";
+            this.btnUpdateCount.UseVisualStyleBackColor = true;
+            this.btnUpdateCount.Click += new System.EventHandler(this.btnUpdateCount_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(53, 399);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // lblNewCount
+            // 
+            this.lblNewCount.AutoSize = true;
+            this.lblNewCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNewCount.Location = new System.Drawing.Point(49, 369);
+            this.lblNewCount.Name = "lblNewCount";
+            this.lblNewCount.Size = new System.Drawing.Size(87, 20);
+            this.lblNewCount.TabIndex = 3;
+            this.lblNewCount.Text = "New Count";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNewCount);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnUpdateCount);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form";
+            this.Text = "Form";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnUpdateCount;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblNewCount;
+    }
+}
+
---
+++ ISIT310-HW5/ISIT310-HW5/Form.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ISIT310_HW5
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        BirdsDataContext birdsCollections = new BirdsDataContext();
+
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            DataLoad();
+        }
+
+        private void DataLoad()
+        {
+            var birds = from myBirdCounts in birdsCollections.BirdCounts
+                orderby myBirdCounts.BirdID ascending
+                select new
+                {
+                    myBirdCounts.CountID,
+                    myBirdCounts.Bird.Name,
+                    myBirdCounts.CountDate,
+                    myBirdCounts.Counted,
+                    myBirdCounts.Bird.Description
+                };
+
+            dataGridView1.DataSource = birds;
+        }
+
+        private void btnUpdateCount_Click(object sender, EventArgs e)
+        {
+            int selectedCountID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["CountID"].Value);
+
+            var selectedBirdCount = (from myBirdCounts in birdsCollections.BirdCounts
+                where myBirdCounts.CountID == selectedCountID
+                select myBirdCounts).Single();
+
+            int newCount = Convert.ToInt32(textBox1.Text);
+
+            selectedBirdCount.Counted = newCount;
+            try
+            {
+                birdsCollections.SubmitChanges();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating row: " + ex.Message);
+            }
+            DataLoad();
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            int selectedCountID = Convert.ToInt32(dataGridView1.CurrentRow.Cells["CountID"].Value);
+
+            var selectedBirdCount = (from myBirdCounts in birdsCollections.BirdCounts
+                                     where myBirdCounts.CountID == selectedCountID
+                                     select myBirdCounts).Single();
+
+            textBox1.Text = selectedBirdCount.Counted.ToString();
+        }
+    }
+}
